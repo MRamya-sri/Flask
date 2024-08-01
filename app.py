c1f3884f29ced60ba76c5ac1beb962a87a9d1a18
MRamya-sri <rsri40170@gmail.com>
2024-08-01 18:27:40 +0530

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app= Flask(__name__)
 
@@ -8,3 +8,4 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
---
+++ static/css/main.js
@@ -0,0 +1,9 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const alertButton = document.getElementById('alertButton');
+    alertButton.addEventListener('click', function() {
+        alert('Button clicked!');
+    });
+});
+
